30f7f4860621042d0bd1d9ed9d82081081935431
kadegno <akad@live.be>
2016-12-20 16:56:37 +0000

    Add backend side

---
+++ back-end/routes/users.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const router = require('express').Router();;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
