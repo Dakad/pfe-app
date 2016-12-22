b0e47ab66a648d2423ee1fe3c2a8d3ffcb786b45
Dakad <akad@live.be>
2016-12-22 23:11:28 +0100

    Backend - Correction

--- backend/modules/config.js
+++ backend/modules/config.js
@@ -26,26 +26,26 @@
  */
 
 // Built-in
-const Promise   = require("promise");
-const dconf     = require('dotenv');
-const nconf     = require('nconf');
-const URL       = require('url');
-const uuidV4    = require('uuid/v4');
+const Promise = require("promise");
+const dconf = require('dotenv');
+const nconf = require('nconf');
+const URL = require('url');
+const uuidV4 = require('uuid/v4');
 
 // Custom - Mine
-const logger    = require("./logger.js")
+const logger = require("./logger.js")
 
 
 
 
-const dbUrlParseur = function (cb){
+const dbUrlParseur = function(cb) {
     const DB_URL = URL.parse(process.env.DATABASE_URL);
     const DB_AUTH = DB_URL.auth.split(':');
 
     nconf.overrides({
-        'DB_Config':{
+        'DB_Config': {
             user: DB_AUTH[0], //env var: DATABASE_USER
-            password: DB_AUTH[1],   //env var: DATABASE_PASSWORD
+            password: DB_AUTH[1], //env var: DATABASE_PASSWORD
             host: DB_URL.hostname, // Server hosting the postgres database
             port: DB_URL.port, //env var: DATABASE_PORT
             database: DB_URL.pathname.split('/')[1],
@@ -63,18 +63,19 @@ const dbUrlParseur = function (cb){
 
 // Methods
 module.exports = {
-    load : function(){
-        return new Promise(function (fulfill, reject){
-            try{
+    load: function() {
+        return new Promise(function(fulfill, reject) {
+            try {
                 logger.info('[Config] Load .env vars into process.env');
                 dconf.load(); // LOAD my .env files into process.env
                 nconf.env(); // LOAD All process into nconf
                 dbUrlParseur();
+                nconf.defaults({ 'PORT': 3030 });
                 // If not env. var TOKENT_SECRET, define a new one.
-                nconf.defaults({'TOKEN_SECRET' : uuidV4() });
-                return fulfill();   // Done, next task on the promise
-            }catch(ex) {
-                return reject(new Error('[DB] Error while loading all configs - '+ex.message))
+                nconf.defaults({ 'TOKEN_SECRET': uuidV4() });
+                return fulfill(); // Done, next task on the promise
+            } catch (ex) {
+                return reject(new Error('[DB] Error while loading all configs - ' + ex.message))
             }
         });
     }
@@ -82,6 +83,3 @@ module.exports = {
 
 
 };
-
-
-
